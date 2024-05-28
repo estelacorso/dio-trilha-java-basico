e08ba41687a74e148e437713946dfc6ebf5a21c5
estelacorso <estelacorso.web@gmail.com>
2024-05-27 21:21:52 -0300

    controle-candidatos

--- controle-candidatos/src/ProcessoSeletivo.java
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -1,10 +1,43 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
+
     public static void main(String[] args) {
-        selecaoCandidatos();
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "pAULO", "AUGUSTO"};
+        for(String candidato: candidatos){
+            entrandoEmContato(candidato);
+        }
+}
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu=false;
+        do{
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+                tentativasRealizadas++;
+            else
+            System.out.println("Contato realizado com sucesso!");
+
+        }while(continuarTentando && tentativasRealizadas<3);
+
+        if(atendeu)
+            System.out.println("Conseguimos contato com " + candidato + "na " + tentativasRealizadas + " tentativa");
+        else
+        System.out.println("Não conseguimos contato com " + candidato + ", numero máximo de tentativas " + tentativasRealizadas + " realizadas");
     }
 
+    //metodo auxiliar
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
+    }
+
+    /*public static void main(String[] args) {
+        selecaoCandidatos();
+    }
+*/
     static void imprimirSelecionados(){
         String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "pAULO", "AUGUSTO"};
 
